f5678173c95299765d9999ca18a10b186ceb9568
Hammad Habib <hammadhabib80@gmail.com>
2023-08-31 11:07:15 +0500

    Fixed the test cases

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def test_add():
     assert 4 == add(2,2)
     
 def test_multiply():
-    assert 4 == add(2,2)
+    assert 4 == multiply(2,2)
     
 def test_divide():
-    assert 1 == add(2,2)    
+    assert 1 == divide(2,2)    
