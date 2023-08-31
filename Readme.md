11ef06697c3aa48ce81e8a1c934056159b9f8b99
Mahnoor <mahnoorakhtar9015@gmail.com>
2023-08-31 11:13:15 +0500

    mahnoor's initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+1-Create a virtual environment `python -m venv ~/.ci_cd_pipeline`
+
+2-Source the virtual environment `source ~/.ci_cd_pipeline/bin/activate`
+
+3-Install the packages required for execution make install
--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 from functions import add
-from functions import multiply
-from functions import divide
+
 def test_add():
     assert 4 == add(2,2)
     
-def test_multiply():
-    assert 4 == add(2,2)
-    
-def test_divide():
-    assert 1 == add(2,2)    
