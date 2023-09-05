a2cfaf8247e0613b0ef30a75c9ec990a2c8e9f57
Muhammad Abdullah Cheema <notabdullah1025@gmail.com>
2023-09-06 00:58:50 +0500

    Again changed test.py file

---
+++ test.py
@@ -0,0 +1,13 @@
+from functions import divide
+
+
+def test_divide():
+    assert 2 == divide(4, 2)
+
+
+def test_add():
+    assert 4 == add(2, 2)
+
+
+def test_multiply():
+    assert 4 == multiply(2, 2)
