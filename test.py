cc732b93aaa01ffc29d695ae633b173377c3b73c
Hammad Habib <hammadhabib80@gmail.com>
2023-09-05 20:40:46 +0500

    Corrected the linting error generated by flake8

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-# from functions import add
 from functions import multiply
-# from functions import divide
-# def test_add():
-#     assert 4 == add(2,2)
-    
+
 def test_multiply():
-    assert 4 == multiply(2,2)
-    
-# def test_divide():
-#     assert 1 == divide(2,2)    
+    assert 4 == multiply(2, 2)
